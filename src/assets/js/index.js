59b8bae638fddcdbba8882c3ddb1c4588d6c7938
Creat Dev <tourrettes83440@gmail.com>
2025-01-03 20:03:10 +0100

    1.0.3

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Minecraft est une liberter pour les cube", "author": "Creat" },
+            { "message": "Bonjour", "author": "Creat" },
+            { "message": "Si tu as un probleme en pvp utilise le bac a sable", "author": "Creat" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
